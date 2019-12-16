dddca1c5db338b568a0ca9b403826e0138a60748
katerinaelsasser <k.elsasser@aol.co.uk>
2019-12-16 19:22:55 +0000

    changing the name of the index html file

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
-
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'film_database'
-app.config["MONGO_URI"] = 'mongodb+srv://kit_22:Milo0104@filmreviews-dqtff.mongodb.net/film_database?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = 'mongodb+srv://kit_22:MiloCat0104@filmreviews-dqtff.mongodb.net/film_database?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
@@ -14,24 +13,6 @@ mongo = PyMongo(app)
 @app.route('/get_films')
 def get_films():
     return render_template("films.html", films=mongo.db.films.find())
-
-
-if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
-
-app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
-
-mongo = PyMongo(app)
-
-
-@app.route('/')
-@app.route('/get_films')
-def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
 if __name__ == '__main__':

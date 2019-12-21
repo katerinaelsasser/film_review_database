dbd2949bbb2ab3c7004841b4214fdb66d778fac3
katerinaelsasser <k.elsasser@aol.co.uk>
2019-12-21 16:51:47 +0000

    adding cards to the index page

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ from flask import Flask, redirect, render_template, request, url_for
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'film_database'
 app.config["MONGO_URI"] = os.getenv(
-                        'MONGO_URI', 
-                        'mongodb+srv://kit_22:Nsos2015@filmreviews-dqtff.mongodb.net/test?retryWrites=true&w=majority')
+'MONGO_URI', 'mongodb+srv://kit_22:Nsos2015@filmreviews-dqtff.mongodb.net/test?retryWrites=true&w=majority')
 
 mongo = PyMongo(app)
 

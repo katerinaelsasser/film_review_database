c8996bd150096c9291b7e50ff91de737e3f00599
katerinaelsasser <k.elsasser@aol.co.uk>
2019-12-21 16:09:20 +0000

    adding a base file and adjusting the index file

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'film_database'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://kit_22:Nsos2015@filmreviews-dqtff.mongodb.net/test?retryWrites=true&w=majority')
+app.config["MONGO_URI"] = os.getenv(
+                        'MONGO_URI', 
+                        'mongodb+srv://kit_22:Nsos2015@filmreviews-dqtff.mongodb.net/test?retryWrites=true&w=majority')
 
 mongo = PyMongo(app)
 

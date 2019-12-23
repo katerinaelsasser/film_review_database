758c4f0c5f02e87f7428ec6d1d924ba7cc9020be
katerinaelsasser <k.elsasser@aol.co.uk>
2019-12-23 21:45:42 +0000

    changing the app to fix error

--- app.py
+++ app.py
@@ -1,28 +1,15 @@
 import os
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'film_database'
-app.config["MONGO_URI"] = os.getenv(
-'MONGO_URI', 'mongodb+srv://kit_22:Nsos2015@filmreviews-dqtff.mongodb.net/test?retryWrites=true&w=majority')
 
-mongo = PyMongo(app)
 
-# Home
 @app.route('/')
-def home_page():
-        return render_template(
-                'index.html',
-                active='home',
-                title="Home")
+def hello():
+    return 'Hello World ...again'
 
-# Review
-@app.route('/review)
-def review_page():
-        return render_template(
-                'review.html',
-                active='review',
-                title="Review")
 
 if __name__ == '__main__':
-        app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)

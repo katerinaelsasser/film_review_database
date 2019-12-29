91f499ca30cf0a4a6182a9852bfece5111828311
katerinaelsasser <k.elsasser@aol.co.uk>
2019-12-29 12:30:50 +0000

    adjusting the app file

--- app.py
+++ app.py
@@ -10,6 +10,4 @@ def hello():
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
+    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)

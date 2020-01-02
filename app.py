ec24bb89dcae4d339e88f820e9750925a0fc45aa
katerinaelsasser <k.elsasser@aol.co.uk>
2020-01-02 20:18:35 +0000

    adding more python code to link mongodb

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
+# import section
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 import os
-from flask import Flask
 
+#mongodb
 app = Flask(__name__)
+app.config[
+    'MONGO_URI'] = 'mongodb+srv://kit_22:Nsos2015@filmreviews-dqtff.mongodb.net/test?retryWrites=true&w=majority'
+app.config['MONGO_DBNAME'] = 'DataCentricLocksProject'
+mongo = PyMongo(app)
 
-
+#connecting the index file
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
-
+@app.route('/index')
+def index():
+    context = mongo.db.Locks.find()
+    return render_template('index.html', locks=context)
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)
+    app.run(host='0.0.0.0',
+            port=(os.environ.get('PORT')),
+            debug=True)

c47663c35cc4d8a673f97e80a5717b0fa5949bd5
katerinaelsasser <k.elsasser@aol.co.uk>
2020-01-05 12:57:29 +0000

    adjusting the link on te env/app page to connect

--- app.py
+++ app.py
@@ -2,13 +2,14 @@
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-import os
+from os import path
+if path.exists("env.py"):
+  import env 
 
 #mongodb
 app = Flask(__name__)
-app.config[
-    'MONGO_URI'] = 'mongodb+srv://kit_22:Nsos2015@filmreviews-dqtff.mongodb.net/test?retryWrites=true&w=majority'
-app.config['MONGO_DBNAME'] = 'DataCentricLocksProject'
+app.config['MONGO_URI'] = os.environ["MONGO_URI"]
+app.config['MONGO_DBNAME'] = app.config['MONGO_DBNAME']
 mongo = PyMongo(app)
 
 #connecting the index file
@@ -19,6 +20,6 @@ def index():
     return render_template('index.html', locks=context)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',
-            port=(os.environ.get('PORT')),
+    app.run(host=os.environ.get(os.environ["IP"])),
+            port=(os.environ.get(os.environ["PORT"])),
             debug=True)

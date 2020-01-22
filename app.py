aff34c7de1d6250d757d52ab7c32d05e34a99586
katerinaelsasser <k.elsasser@aol.co.uk>
2020-01-22 19:55:53 +0000

    removing an unused python code

--- app.py
+++ app.py
@@ -24,25 +24,9 @@ def home_page():
 def get_films():
     return render_template("film_listing.html")
 
-#Review Page
 @app.route("/add_review")
 def add_review():
     return render_template("add_review.html") 
-
-@app.route('/update_film/<film_id>', methods=["POST"])
-def update_film(film_id):
-    film = mongo.db.film
-    film.update( {'_id': ObjectId(film_id)},
-    {
-        'film_name':request.form.get('film_name'),
-        'film_year':request.form.get('film_year'),
-        'film_director': request.form.get('film_director'),
-        'film_genre': request.form.get('film_genre'),
-        'film_rating': request.form.get('film_rating'),
-        'film_length': request.form.get('film_length'),
-        'film_description': request.form.get('film_description'),
-    })
-    return redirect(url_for('get_films'))       
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),

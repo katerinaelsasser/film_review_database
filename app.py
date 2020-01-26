d7e00add65fbc99d4a81929d4f818b3a4cdf5247
katerinaelsasser <k.elsasser@aol.co.uk>
2020-01-26 21:02:38 +0000

    adding template folders and updating pages

--- app.py
+++ app.py
@@ -14,23 +14,16 @@ app.config['MONGO_DBNAME'] = os.environ['MONGO_DBNAME']
 mongo = PyMongo(app)
 
 #Home Page
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/homepage', methods=['GET', 'POST'])
 def homepage():
     return render_template("index.html") 
 
-#Film Database
+#viewfilms page
 @app.route('/viewfilms')
 def viewfilms():
     return render_template("viewfilms.html")
 
-@app.route("/viewreviews")
-def viewreviews():
-    return render_template("viewreviews.html") 
-
-@app.route("/addreviews")
-def addreviews():
-    return render_template("addreview.html") 
-    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),

5dfad5329a735556031a440c87d3fe3294691d0e
katerinaelsasser <k.elsasser@aol.co.uk>
2020-01-26 15:01:21 +0000

    adding header images and updating js link

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -96,5 +96,5 @@ function carousel() {
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 2000); // Change image every 2 seconds
+  setTimeout(carousel, 2000); 
 }

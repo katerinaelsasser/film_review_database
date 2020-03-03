44304591e73924fce6bd8e863a3b19d0c278b23c
katerinaelsasser <k.elsasser@aol.co.uk>
2020-03-03 22:22:10 +0000

    adding mongdb to the js link

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -1,10 +1,16 @@
 function getanswers(film) {
-$.get("http://www.omdbapi.com/?i=tt3896198&apikey=8fb317f8")
-    let rawstring=JSON.stringify(rawdata);
-    data=JSON.parse(rawstring)
-    let title = data.Search[0].Title;
-    let year = data.Search[0].Year;
-    let posterurl = data.Search0[0].Poster;
-    document.getElementById('film-results').innerHTML="
-    "<div>"+"<img scr="+posterurl+"><h3>"+title+" ( "+year+" )</h3>";
- }
+fetch("mongodb+srv://kit_22:MONGO_URI@filmreviews-dqtff.mongodb.net/test?retryWrites=true&w=majority)
+.then(res => res.json())
+    .then(data => {
+      console.log(data);
+      data.Search.forEach(element => {
+ let movieInfo = "<div><div id="${film._ID}">
+        <div>
+          <img src="${film.film_poster}">
+          <span class="card-title">${film.film_name}</span>
+        </div>";
+        $('#movies').appendTo(movieInfo);
+      });
+    });
+}
+}

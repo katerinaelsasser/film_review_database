f90688098ba42aada8e2ed97774d01615da78c59
katerinaelsasser <k.elsasser@aol.co.uk>
2020-03-03 21:59:44 +0000

    adding filmdisplay to viewfilms

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -6,4 +6,5 @@ $.get("http://www.omdbapi.com/?i=tt3896198&apikey=8fb317f8")
     let year = data.Search[0].Year;
     let posterurl = data.Search0[0].Poster;
     document.getElementById('film-results').innerHTML="
-    "<div>"+"<img scr="+posterurl+"><h3>"+title+" ( "+year+" )</h3>"; });
+    "<div>"+"<img scr="+posterurl+"><h3>"+title+" ( "+year+" )</h3>";
+ }

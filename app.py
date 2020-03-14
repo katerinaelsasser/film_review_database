f2115ba9e5459de1892a58da5e71637887e7010b
katerinaelsasser <k.elsasser@aol.co.uk>
2020-03-14 15:15:19 +0000

    adding addfilm python code

--- app.py
+++ app.py
@@ -44,6 +44,16 @@ def insertreview():
     reviews.insert_one(request.form.to_dict())
     return redirect(url_for('viewreviews'))
 
+#addfilms page
+@app.route('/addfilms', methods=['GET'])
+def addreviews():
+    return render_template("pages/addfilm.html")
+
+@app.route('/insertfilm', methods=['POST'])
+def insertfilm():
+    films =  mongo.db.films
+    films.insert_one(request.form.to_dict())
+    return redirect(url_for('viewfilms'))
 
 #subscribe page
 @app.route('/subscribe')

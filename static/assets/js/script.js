f0d31d6e41864a6bdf102dd97876dbb226a24077
katerinaelsasser <k.elsasser@aol.co.uk>
2020-03-15 16:01:05 +0000

    changing links on the navbars

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -1,31 +1,15 @@
 //subscribe modal
 var modal = document.getElementById("submodal");
 var btn = document.getElementById("subscribebtn");
-var span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
   modal.style.display = "block";
 }
+var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 }
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
-  }
-}
-
-//login modal
-var logmodal = document.getElementById("loginmodal");
-var btn = document.getElementById("loginbtn");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
-  logmodal.style.display = "block";
-}
-span.onclick = function() {
-  logmodal.style.display = "none";
-}
-window.onclick = function(event) {
-  if (event.target == logmodal) {
-    logmodal.style.display = "none";
   }
 }

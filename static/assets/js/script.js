33936b728e10e89008402814fda1d65f180172ac
katerinaelsasser <k.elsasser@aol.co.uk>
2020-03-15 15:17:18 +0000

    adjusting login & subscribe modals

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -1,23 +1,29 @@
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
+//subscribe modal
+var modal = document.getElementById("submodal");
 var btn = document.getElementById("subscribebtn");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
-// When the user clicks anywhere outside of the modal, close it
+//login modal
+var modal = document.getElementById("loginmodal");
+var btn = document.getElementById("loginbtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";

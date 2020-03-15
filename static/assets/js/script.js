a72af2fa1106e904183d3283a38e338f9d735971
katerinaelsasser <k.elsasser@aol.co.uk>
2020-03-15 13:02:31 +0000

    adding a subscribe modal

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -1,16 +1,25 @@
-function getanswers(film) {
-fetch("mongodb+srv://kit_22:MONGO_URI@filmreviews-dqtff.mongodb.net/test?retryWrites=true&w=majority)
-.then(res => res.json())
-    .then(data => {
-      console.log(data);
-      data.Search.forEach(element => {
- let movieInfo = "<div><div id="${film._ID}">
-        <div>
-          <img src="${film.film_poster}">
-          <span class="card-title">${film.film_name}</span>
-        </div>";
-        $('#movies').appendTo(movieInfo);
-      });
-    });
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("subscribebtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
 }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }

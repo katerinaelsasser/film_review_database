85eb9915cfaf9a7aac05987d3a5e375a8408c9e2
katerinaelsasser <k.elsasser@aol.co.uk>
2020-04-01 18:47:50 +0000

    adding genre modal

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -1,16 +1,15 @@
-//film form (genre colapse)
-
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
+//film form (genre modal)
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }

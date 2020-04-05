ec1b26929d41502f1eb5c1136542dfc4b3a1e813
katerinaelsasser <k.elsasser@aol.co.uk>
2020-04-05 17:16:34 +0000

    changing the js & view film layout

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -1,21 +1,17 @@
 //search bar
 function bob(userSearch) {
     fetch('https://www.omdbapi.com/?j=tt3896198&apikey=8f317f8&plot=ful=$(userSearch)&type=movie')
-    .then(response=>response.json())
-    .then(searchResults=>{
-        searchResults.Search.ForEach(movie=>{
-        if(movie.Poster ==="N/A")(
-        let poster = "assets/noPOSTER.jpg"
-        )else{
-            poster = movie.Poster
-        }
-    $("#movieDIV").append(
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      data.Search.forEach(element => {
+        let movieInfo = `
         '<div class="col-md-3 film-card">
         <div class="card border-0 shadow">
             <img src="{poster}" class="card-img-top" alt="movie-poster">
             <p>"${movie.Title}"</p>
-            </div>'
-    );
+            </div>`;
+        $('#moviesDIV').appendTo(movieInfo);
+      });
     });
-});
 }

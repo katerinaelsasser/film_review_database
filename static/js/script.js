760b2253205efe06f74fef634815f661acd03368
katerinaelsasser <k.elsasser@aol.co.uk>
2020-04-21 17:16:53 +0000

    removing newarrival code

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 //search bar
 function filmSearch() {
     var userSearchBar = document.getElementById('filmSearchBar').value;
-    // clears before generating
+// clears search before generating
     $('#moviesDIV').empty();
 //omdb api code
     fetch('https://www.omdbapi.com/?apikey=fe2afef6&s='+userSearchBar+'&type=movie')
@@ -10,12 +10,12 @@ function filmSearch() {
 console.log(data.Search);
       data.Search.forEach(movie => {
 console.log(movie.Title);
-//displays the films
+//display for each film
         let movieInfo =
         `<div class="col-md-3 film-card">
         <div class="card border-0 shadow">
         <a href="/view/movies/${movie.imdbID}">
-            <img src="${movie.Poster}" class="card-img-top new-arrivals" alt="movie-poster"></a>
+            <img src="${movie.Poster}" class="card-img-top" alt="movie-poster"></a>
             <p>${movie.Title}</p>
             </div>`;
         $('#moviesDIV').append(movieInfo);

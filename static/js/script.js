ce6993716899dcea2670c32d5822c2789372c6de
katerinaelsasser <k.elsasser@aol.co.uk>
2020-04-28 21:24:23 +0000

    adding individual js code

--- static/js/script.js
+++ static/js/script.js
@@ -11,7 +11,7 @@ console.log(movie.Title);
         let moviePreview =
         `<div class="col-md-3 film-card film-effect">
         <div class="card border-0 shadow">
-        <a href="/view/movies/${movie.imdbID}">
+        <a onclick="movieSelected('${movie.imdbID}')" href="#">
             <img src="${movie.Poster}" class="card-img-top" alt="movie-poster"></a>
             <h4 class="text-center film-title">${movie.Title}</h4>
             </div>`;
@@ -26,17 +26,24 @@ $("#filmSearchBar").change(function () {
 
 const userSearchBar = $("#filmSearchBar")
 
+
+
 //individual page
-function filmview() {
-var movie_id = document.getElementById('movie_id').value;
- fetch('https://www.omdbapi.com/?apikey=fe2afef6&i='+movie_id)
- .then(res => res.json())
-    .then(data => {
-console.log(data.Search);
-      data.Search.forEach(movie => {
-console.log(movie.imdbID);
-        let movieInfo =   
-        `<div class="col-md-9 col-lg-8 mx-auto">
+function movieSelected(id){
+  sessionStorage.setItem('movieId', id);
+  window.location = 'pages/individualfilm.html';
+  return false;
+}
+
+function filmview(){
+  let movieId = sessionStorage.getItem('movieId');
+  axios.get('https://www.omdbapi.com/?apikey=fe2afef6&i='+movieId)
+    .then((response) => {
+      console.log(response);
+      let movie = response.data;
+
+      let movieInfo =
+      `<div class="col-md-9 col-lg-8 mx-auto">
               <h3 class="login-heading mb-4">${movie.Title}</h3>
                 <div>
                 ${movie.Year} | ${movie.Rating}
@@ -54,8 +61,7 @@ console.log(movie.imdbID);
             <div class="d-none d-md-flex col-md-4 col-lg-6 bg-image">
     <img src="${movie.Poster}">
     </div>`;
-        $('#Film_ID').append(movieInfo);
-      });
-    });
-}
 
+      $('#Film_ID').html(movieInfo);
+    })
+}

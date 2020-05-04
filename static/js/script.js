a5bafea49e43f57fcc664d7926b4b03e107c55b8
katerinaelsasser <k.elsasser@aol.co.uk>
2020-05-04 20:09:49 +0000

    adding different modals

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,45 @@
-//modal
+//info modal
 
 var modal = document.getElementById("infoModal");
 var btn = document.getElementById("infoBtn");
-var span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
   modal.style.display = "block";
-}
-span.onclick = function() {
-  modal.style.display = "none";
 }
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+}
+
+//review modal
+
+var reviewModal = document.getElementById("reviewModal");
+var reviewBtn = document.getElementById("reviewBtn");
+reviewBtn.onclick = function() {
+  reviewModal.style.display = "block";
+}
+window.onclick = function(event) {
+  if (event.target == reviewModal) {
+    reviewModal.style.display = "none";
+  }
+}
+
+//edit modal
+
+var editModal = document.getElementById("editModal");
+var editBtn = document.getElementById("editBtn");
+editBtn.onclick = function() {
+  editModal.style.display = "block";
+}
+window.onclick = function(event) {
+  if (event.target == editModal) {
+    editwModal.style.display = "none";
+  }
+}
+
+//close modal
+
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+  modal.style.display = "none";
 }

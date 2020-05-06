76ee57f6258fcaa6e9433142e6ccf43ba7820667
katerinaelsasser <k.elsasser@aol.co.uk>
2020-05-06 21:03:48 +0000

    changing link to editmovie

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 //info modal
 
-var modal = document.getElementById("infoModal");
-var btn = document.getElementById("infoBtn");
+var modal = document.getElementById("modal");
+var btn = document.getElementById("modalBtn");
 var span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
   modal.style.display = "block";
